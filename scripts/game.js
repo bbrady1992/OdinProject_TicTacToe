2f1de8ce31af12be5c25c937a660fc5d91df5100
Ben Brady <benbradypm@protonmail.com>
2021-07-14 14:20:12 -0400

    Working on setting up board and player info

--- scripts/game.js
+++ scripts/game.js
@@ -1,20 +1,37 @@
 const Player = (mark) => {
   const getMark = () => mark;
+
+  return {getMark};
 };
 
 const gameboard = (function () {
-  const _board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-  const report = () => console.log("Herp");
-  const setPlayers = (player1, player2) => {};
+  const _board = ["X", "X", "X", "X", "X", "X", "X", "X", "O"];
 
-  const playerWins = () => {
-    // TODO (bbrady) - implement this
-  };
+  const renderBoard = () => {
+    for (let i = 1; i <= _board.length; ++i) {
+      document.querySelector(`#space-${i}`).textContent = _board[i - 1];
+    }
+  }
 
   return {
-    report,
-    setPlayers,
+    renderBoard
   };
 })();
 
-gameboard.report();
+const game = (function () {
+  const _players = [Player("X"), Player("O")];
+  const _activePlayer = _players[0];
+
+  function renderPlayerInfo() {
+    console.log("Rendering player info");
+    const player1Info = document.querySelector("#player1");
+    player1Info.textContent = `Player 1 ("${_players[0].getMark()}")`;
+    const player2Info = document.querySelector("#player2");
+    player2Info.textContent = `Player 2 ("${_players[1].getMark()}")`;
+  };
+
+  return {renderPlayerInfo, _players};
+})();
+
+gameboard.renderBoard();
+game.renderPlayerInfo();
