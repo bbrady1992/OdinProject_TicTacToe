a4af9560bd44e4ffc846185ceb6463e1dad4061b
Ben Brady <benbradypm@protonmail.com>
2021-12-28 15:04:06 -0500

    Placing markers and reset working
    Not checking for wins yet

--- scripts/game.js
+++ scripts/game.js
@@ -1,26 +1,34 @@
 const Player = (mark) => {
   const getMark = () => mark;
 
-  return {getMark};
+  return { getMark };
 };
 
 const gameboard = (function () {
-  const _board = ["X", "X", "X", "X", "X", "X", "X", "X", "O"];
+  const _board = ["", "", "", "", "", "", "", "", ""];
 
   const renderBoard = () => {
     for (let i = 1; i <= _board.length; ++i) {
       document.querySelector(`#space-${i}`).textContent = _board[i - 1];
     }
-  }
+  };
+
+  const resetBoard = () => {
+    document
+      .querySelectorAll('*[id^="space-"]')
+      .forEach((s) => (s.textContent = ""));
+  };
+
+  document.querySelector("#reset-button").addEventListener("click", resetBoard);
 
   return {
-    renderBoard
+    renderBoard,
   };
 })();
 
 const game = (function () {
   const _players = [Player("X"), Player("O")];
-  const _activePlayer = _players[0];
+  let _activePlayerIndex = 0;
 
   function renderPlayerInfo() {
     console.log("Rendering player info");
@@ -28,9 +36,28 @@ const game = (function () {
     player1Info.textContent = `Player 1 ("${_players[0].getMark()}")`;
     const player2Info = document.querySelector("#player2");
     player2Info.textContent = `Player 2 ("${_players[1].getMark()}")`;
-  };
+  }
 
-  return {renderPlayerInfo, _players};
+  function _changeActivePlayer() {
+    _activePlayerIndex = (_activePlayerIndex + 1) % 2;
+  }
+
+  let squares = document.querySelectorAll('*[id^="space-"]');
+  console.log({ squares });
+  squares.forEach((s) =>
+    s.addEventListener("click", (e) => {
+      if (e.target.textContent === "") {
+        e.target.textContent = _players[_activePlayerIndex].getMark();
+        _changeActivePlayer();
+      }
+    })
+  );
+
+  document
+    .querySelector("#reset-button")
+    .addEventListener("click", () => (_activePlayerIndex = 0));
+
+  return { renderPlayerInfo, _players };
 })();
 
 gameboard.renderBoard();
